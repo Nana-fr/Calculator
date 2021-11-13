a43768da2b3ae3d16090e1681bf74ec39c953a6b
Anaïs <c7anais@gmail.com>
2021-11-13 17:16:52 +0100

    disable click on operators when already click

--- js/main.js
+++ js/main.js
@@ -4,16 +4,23 @@ let buttons = document.getElementsByClassName("value");
 let operators = document.getElementsByClassName("operator");
 let result = "";
 
+// numbers
 for (let button of buttons) {
     button.onclick=function() {
         if (calculation.innerText.includes("=")) {
             calculation.innerText = "";
             output.innerText = "";
         }
+    let oneTime=document.querySelectorAll(".oneTime")
+    oneTime.forEach(function(oneTime) {
+        oneTime.classList.remove("oneTime");
+    })
     output.innerText += this.value;
     calculation.innerText += this.value;
     }
 };
+
+// operators
 
 for (let operator of operators) {
     operator.onclick=function() {
@@ -24,21 +31,29 @@ for (let operator of operators) {
     result += output.innerText + operator.value;
     calculation.innerText += operator.value;
     output.innerText="";
-    console.log(result + " " + result.innerText);
+    let notNum=document.querySelectorAll(".notNum")
+        notNum.forEach(function(notNum) {
+            notNum.classList.add("oneTime");
+        });
     }
 };
 
+// final result
 document.getElementById("submit").onclick=function(){
     calculation.innerText += "=";
     output.innerText = eval(result+output.innerText);
     result = "";
+    document.getElementById("delete").classList.add("oneTime");
+    document.getElementById("clear").classList.add("oneTime");
+
 };
+
+// delete and clear
 
 document.getElementById("delete").onclick=function(){
     if (result.endsWith("-" || "+")) {
         result = result.substr(0, result.length-1);
     }
-    console.log(result + " " + result.innerText);
     calculation.innerText = calculation.innerText.substring(0, calculation.innerText.length-1)
     output.innerText = output.innerText.substring(0, output.innerText.length-1);
 };
@@ -54,12 +69,15 @@ document.getElementById("clearAll").onclick=function(){
     result = "";
 };
 
+// special operator
+
 document.getElementById("racine").onclick=function(){
     if (calculation.innerText.includes("=")) {
         calculation.innerText = output.innerText;
     }
     calculation.innerText = calculation.innerText.substr(0, calculation.innerText.length-output.innerText.length) + output.innerText.replace(output.innerText, "√"+output.innerText);
     output.innerText = output.innerText.replace(output.innerText, Math.sqrt(output.innerText));
+    document.getElementById("racine").classList.add("oneTime");
 };
 
 document.getElementById("negatif").onclick=function(){
@@ -68,6 +86,7 @@ document.getElementById("negatif").onclick=function(){
     }
     calculation.innerText = calculation.innerText.substr(0, calculation.innerText.length-output.innerText.length) + output.innerText.replace(output.innerText, "-"+output.innerText);
     output.innerText = output.innerText.replace(output.innerText, -1*(output.innerText));
+    document.getElementById("negatif").classList.add("oneTime");
 };
 
 document.getElementById("carré").onclick=function(){
@@ -76,6 +95,7 @@ document.getElementById("carré").onclick=function(){
     }
     calculation.innerText = calculation.innerText.substr(0, calculation.innerText.length-output.innerText.length) + output.innerText+"^2";
     output.innerText = output.innerText*output.innerText;
+    document.getElementById("carré").classList.add("oneTime");
 };
 
 document.getElementById("1/x").onclick=function(){
@@ -84,4 +104,5 @@ document.getElementById("1/x").onclick=function(){
     }
     calculation.innerText = calculation.innerText.substr(0, calculation.innerText.length-output.innerText.length) + "1/("+output.innerText+")";
     output.innerText = 1/output.innerText;
+    document.getElementById("1/x").classList.add("oneTime");
 };
